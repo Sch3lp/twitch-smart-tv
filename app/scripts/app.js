76e7e6f67c3efb8aa7a51e6a6a2ddaef7e740535
Tim Schraepen <sch3lp@gmail.com>
2013-07-27 22:42:34 +0200

    was able to init Twitch.api in index.html; made some progress on handling asynchronous api calls, but no results yet though :(

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,24 @@ angular.module('twitchSmartTvApp', [])
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          twitchStreams: function($q){
+            var deferred = $q.defer();
+            
+            var myCallback = function(error, data) {
+              if (error === null){
+                deferred.resolve(data);
+              } else {
+                deferred.reject(error);
+              }
+            };
+            // internally uses $ajax({}) to perform asynchronous calls to the Twitch REST API
+            Twitch.api({method: 'streams', params: { game:'StarCraft II: Heart of the Swarm', limit:5}}, myCallback);
+
+            return deferred.promise;
+          }
+        }
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,29 +1,7 @@
 'use strict';
 
 angular.module('twitchSmartTvApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.remoteKeys = ['KEY_LEFT'];
-    Twitch.api({method: 'streams', params: { game: 'StarCraft II: Heart of the Swarm', limit:5}}, function(error, streams) {
-	  $scope.streams = streams;
-	  $scope.error = error;
-	});
-    /*$http.get('https://api.twitch.tv/kraken/streams/api/streams')
-    	.success(function (data){
-	    	$scope.streams = data;
-	    	$scope.loading = false;
-    	})
-    	.error(function (){
-    		$scope.error = "Couldn't load Twitch Streams :(";
-	    	$scope.loading = false;
-    	});*/
-  })
-  // TODO: figure out why I can't just add this as a script tag to index.html
-  .run(function(){
-  	Twitch.init({clientId: '3e4prc5i7sljtycsdsbbxzoqex93yrn', 
-        version: 'v2'},
-        function(error, status) {  
-            if (error) {
-                console.log(error);
-            }
-        });
+  .controller('MainCtrl', function ($q, $scope, $watch, twitchStreams) {
+    // TODO: how do I set my $scope.streams variable to the returned streams array on asynchronous calls? Use AngularJS async ?
+    $scope.streams = twitchStreams;
   });
