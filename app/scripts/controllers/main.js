71b720a9d5f79bb556895b5b2a4b15ecbc173ceb
Tim Schraepen <sch3lp@gmail.com>
2013-07-27 21:03:18 +0200

    finally managed to get a listing of all starcraft2 streams, limited to 5, but I can't seem to assign the twitch response to a .var... yet

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,18 +2,28 @@
 
 angular.module('twitchSmartTvApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.remoteKeys = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $http.get('/api/streams')
-    	.succes(function (data){
+    $scope.remoteKeys = ['KEY_LEFT'];
+    Twitch.api({method: 'streams', params: { game: 'StarCraft II: Heart of the Swarm', limit:5}}, function(error, streams) {
+	  $scope.streams = streams;
+	  $scope.error = error;
+	});
+    /*$http.get('https://api.twitch.tv/kraken/streams/api/streams')
+    	.success(function (data){
 	    	$scope.streams = data;
 	    	$scope.loading = false;
     	})
     	.error(function (){
     		$scope.error = "Couldn't load Twitch Streams :(";
 	    	$scope.loading = false;
-    	});
+    	});*/
+  })
+  // TODO: figure out why I can't just add this as a script tag to index.html
+  .run(function(){
+  	Twitch.init({clientId: '3e4prc5i7sljtycsdsbbxzoqex93yrn', 
+        version: 'v2'},
+        function(error, status) {  
+            if (error) {
+                console.log(error);
+            }
+        });
   });
