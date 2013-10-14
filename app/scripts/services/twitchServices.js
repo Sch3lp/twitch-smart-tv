465bfc6b018d3d391dc898e22a8dc39b5429b9f9
Sch3lp <sch3lp@gmail.com>
2013-10-14 17:00:32 +0200

    Don't call resolve when there's an error, call reject instead.

--- app/scripts/services/twitchServices.js
+++ app/scripts/services/twitchServices.js
@@ -5,21 +5,28 @@ angular.module('twitchServices', [])
         /* Another super useful link: http://stackoverflow.com/questions/15604196/promises-in-angularjs-and-where-to-use-them */
         var deferred = $q.defer();
         var twitchCallBack = function(error, data) {
-           /*
-            * Wrap in $apply so when Angulars model requests an update, it will apply the result of the async twitch call with the resolve on the deferred object.
-            * Just assigning the promise (in the controller) won't update the model by itself.
-            */
-          $rootScope.$apply(function(){
-            /* need to suffix with .streams because original object looks like this:
-            {
-              streams: [{channel: xxx, user: xxx, name: xxx, ...}],
-              _links: {}
-            }
-            See: https://github.com/justintv/Twitch-API/blob/master/v2_resources/streams.md#example-response-1
-            */
-            deferred.resolve(data.streams);
-            deferred.reject(error);
-          });
+          if (error === null){
+            /*
+              * Wrap in $apply so when Angulars model requests an update, it will apply the result of the async twitch call with the resolve on the deferred object.
+              * Just assigning the promise (in the controller) won't update the model by itself, since we're performing this callback outside of Angulars context.
+              * See: http://stackoverflow.com/questions/13962334/can-i-use-jquery-ajax-with-angular-js-promise-api
+              */
+            $rootScope.$apply(function(){
+              /* need to suffix with .streams because original object looks like this:
+              {
+                streams: [{channel: xxx, user: xxx, name: xxx, ...}],
+                _links: {}
+              }
+              See: https://github.com/justintv/Twitch-API/blob/master/v2_resources/streams.md#example-response-1
+              */
+              deferred.resolve(data.streams);
+            });
+          } else {
+            // same deal with .reject, if we don't wrap it in an $apply, Angulars errorhandlers will not be triggered
+            $rootScope.$apply(function(){
+              deferred.reject(error);
+            });
+          }
         };
         // internally uses $ajax({}) to perform asynchronous calls to the Twitch REST API
         Twitch.api({method: 'streams', params: { game:'StarCraft II: Heart of the Swarm', limit:5}}, twitchCallBack);
